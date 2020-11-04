4141dfca71f71612ed9ca608d433d6ad348b9c60
ImChangk <73332578+Changk0128@users.noreply.github.com>
2020-11-04 20:56:43 +0800

    第三次提交

--- oldpeople-provider/src/main/java/com/jk/controller/TopController.java
+++ oldpeople-provider/src/main/java/com/jk/controller/TopController.java
@@ -3,7 +3,9 @@ package com.jk.controller;
 import com.jk.config.PageResult;
 import com.jk.entity.Top;
 import com.jk.service.TopService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -17,14 +19,14 @@ public class TopController {
     *   预约线上表分页 查询
     * */
     @RequestMapping("/findTop")
-    public PageResult findTop(Integer currPage, Integer pageSize, Top top){
+    public PageResult findTop(@RequestParam Integer currPage, @RequestParam Integer pageSize,@RequestBody Top top){
         return topService.findTop(currPage,pageSize,top);
     }
     /*
     *   预约线上表新增
     * */
     @RequestMapping("/addTop")
-    public void addTop(Top top){
+    public void addTop(@RequestBody Top top){
         topService.addTop(top);
     }
 }
--- oldpeople-web/src/main/java/com/jk/service/OldPeopleService.java
+++ oldpeople-web/src/main/java/com/jk/service/OldPeopleService.java
@@ -47,4 +47,40 @@ public interface OldPeopleService {
     public void deleteUnder(@RequestParam Integer underId);
     @RequestMapping("findUnderById")
     public Under findUnderById(@RequestParam Integer underId);
+
+    //老人会员信息表 Changk
+    @RequestMapping("/findPeople")
+    public PageResult findPeople(@RequestParam Integer currPage, @RequestParam Integer pageSize, @RequestBody People people);
+    @RequestMapping("/addPeople")
+    public void addPeople(@RequestBody People people);
+    @RequestMapping("/onePeople")
+    public People onePeople(@RequestParam Integer ids);
+    @RequestMapping("/delOne")
+    public void delOne(@RequestParam Integer ids);
+
+    //老人外出登记表 Changk
+    @RequestMapping("/findOut")
+    public PageResult findOut(@RequestParam Integer currPage, @RequestParam Integer pageSize, @RequestBody Out out);
+    @RequestMapping("/addOut")
+    public void addOut(@RequestBody Out out);
+    @RequestMapping("/oneOut")
+    public Out oneOut(@RequestParam Integer ids);
+    @RequestMapping("/delOut")
+    public void delOut(@RequestParam Integer ids);
+
+    //员工请假详细信息表 Changk
+    @RequestMapping("/findThing")
+    public PageResult findThing(@RequestParam Integer currPage, @RequestParam Integer pageSize, @RequestBody Thing thing);
+    @RequestMapping("/addThing")
+    public void addThing(@RequestBody Thing thing);
+    @RequestMapping("/delThing")
+    public void delThing(@RequestParam Integer ids);
+    @RequestMapping("/oneThing")
+    public Thing oneThing(@RequestParam Integer ids);
+
+    //预约线上信息表 Changk
+    @RequestMapping("/findTop")
+    public com.jk.config.PageResult findTop(@RequestParam Integer currPage, @RequestParam Integer pageSize, @RequestBody Top top);
+    @RequestMapping("/addTop")
+    public void addTop(@RequestBody Top top);
 }

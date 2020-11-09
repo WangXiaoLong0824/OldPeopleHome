536d71996b6f7ab078fe75f15421ffef5ca57f77
weiwei196 <1965725661@qq.com>
2020-11-09 15:31:27 +0800

    ww第四次页面完善

--- oldpeople-web/src/main/java/com/jk/controller/IndexController.java
+++ oldpeople-web/src/main/java/com/jk/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("index")
 public class IndexController {
 
     @RequestMapping("toDormList")
